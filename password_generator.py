e2149b55cd1c63b30830f5d24347d9d4e55f2437
Eduardo Batista <58524485+eduhbg@users.noreply.github.com>
2021-10-07 17:14:18 -0300

    organize functions

--- password_generator.py
+++ password_generator.py
@@ -4,21 +4,7 @@ import random
 CHARACTERS = list(string.ascii_letters + string.digits + string.punctuation)
 FILE_NAME = 'generated_passwords.txt'
 
-def save_data(subject, password):
-    password_file = open(FILE_NAME, 'a')
-    password_file.write(subject + ': ' + password + '\n')
-    password_file.close()
-
-def generate_password(length):
-    password = ''
-
-    for i in range(length):
-        password = password + random.choice(CHARACTERS)
-
-    return password
-
 def main():
-
     while True:
         length = input('Type the length of your password or "stop" \
 to finish.\n')
@@ -35,5 +21,18 @@ is for what.\n')
                 break
             else: print('Invalid input.')
 
-if __name__ == "__main__":
+ def generate_password(length):
+    password = ''
+
+    for i in range(length):
+        password = password + random.choice(CHARACTERS)
+
+    return password
+                
+def save_data(subject, password):
+    password_file = open(FILE_NAME, 'a')
+    password_file.write(subject + ': ' + password + '\n')
+    password_file.close()
+
+if __name__ == '__main__':
     main()
