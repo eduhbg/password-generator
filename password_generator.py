aba340d929c3c0d5bebca4dcda897d720fb11374
eduhbg <eduardohbg@gmail.com>
2022-02-01 22:58:11 -0300

    update some texts and organize the identation

--- password_generator.py
+++ password_generator.py
@@ -4,15 +4,14 @@ import random
 CHARACTERS = list(string.ascii_letters + string.digits + string.punctuation)
 FILE_NAME = 'generated_passwords.txt'
 
+
 def main():
     while True:
-        length = input('Type the length of your password or "stop" \
-to finish.\n')
+        length = input('Type the length of your password or "stop" to finish: ')
 
         try:
             length = int(length)
-            subject = input('Type a subject that will remember that password \
-is for what.\n')
+            subject = input('Type a subject of this password: ')
             password = generate_password(length)
             save_data(subject, password)
         except:
@@ -21,18 +20,21 @@ is for what.\n')
                 break
             else: print('Invalid input.')
 
- def generate_password(length):
+
+def generate_password(length):
     password = ''
 
     for i in range(length):
         password = password + random.choice(CHARACTERS)
 
     return password
-                
+
+
 def save_data(subject, password):
     password_file = open(FILE_NAME, 'a')
     password_file.write(subject + ': ' + password + '\n')
     password_file.close()
+
 
 if __name__ == '__main__':
     main()
